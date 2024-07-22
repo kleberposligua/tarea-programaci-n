d1c30acddfa7b0c5b780c0d2d04e29eab775ac7a
Compu 05 <compu_05@luminar.edu.ec>
2024-07-21 22:32:40 -0500

    tarea de programacion filtre

---
+++ arraycadena.js
@@ -0,0 +1,29 @@
+/*Autor: Jandry Zambrano Palacios
+Fecha: 15/07/2024
+Tema:Determine si el siguiente array 
+tiene cadenas
+ de caracteres con una longitud de 5
+ const cadena = ["hola", "juan", "elefante", "cantar", "teclado", "anona"];
+*/
+//Imprime  el siguiente array en cadenas de carácteres
+const cadena = ["hola", "Juan","elefante","cantar","teclado", "anona"];
+function ImprimirArray(cadena)
+{
+    for( let i=0;i <cadena.length;i++)
+        {
+console.log(cadena[i]);
+        }
+}
+console.log("Imprimiendo el array", cadena);
+ImprimirArray(cadena);
+//Imprime cadena de carácteres con  una longiotud de 5
+cadena.forEach( (cadena) =>{
+console.log(cadena*5);
+
+});
+//Diseñe la funcion que imprima un array
+const imprimevector = ((vector) =>{
+vector.forEach(intem =>{
+   console.log 
+})
+});
